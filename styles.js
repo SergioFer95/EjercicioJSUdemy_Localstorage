a4cce1ef191dcef819a3f0e233f3ed822849063e
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-12-15 10:54:40 +0100

    Add custom names to work with them in our javascript´s file

--- styles.js
+++ styles.js
@@ -1,10 +1,13 @@
 'use strict';
 
-window.addEventListener("load", () =>{
+window.addEventListener("load", () => {
 
+    var form = document.querySelector('[customName="formOne"]');
 
-
-
-
+    form.addEventListener("submit", () => {
     
+        var userFilms = document.querySelector('[customName="inputText"]').value;
+        localStorage.setItem("Films", userFilms);
+    })
 });
+
